f5348cfbe6d2f0c18c63ddbf98730081659be43b
Christopher <reinkendigital@gmail.com>
2020-05-18 16:31:56 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,5 @@
 class Student < ActiveRecord::Base
     def to_s
-        @name = []
-        @name << self.first_name
-        @name << self.last_name
-        @name.join(" ")
+        self.first_name + " " + self.last_name
     end
 end
